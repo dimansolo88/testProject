b0192b774933b8b0f76ca9dd61beb64b8f5f77f2
dimansolo <dimansolo84@gmail.com>
2019-06-10 16:21:29 +0300

    added comDidMounte

--- src/components/Navbar/Users/Users.jsx
+++ src/components/Navbar/Users/Users.jsx
@@ -5,9 +5,9 @@ import photos from "../../../assets/images/userPhoto.jpg"
 
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+   
 
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items)
