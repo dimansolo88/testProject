e3a3418e528f893acf71d25a6394caa6afe7963e
dimansolo <dimansolo84@gmail.com>
2019-07-29 15:48:43 +0300

    added login on server

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import {LoginReduxForm} from "./LoginForm";
+import style from "./login.module.css"
+import {connect} from "react-redux";
+import {loginThunkCreator} from "../../redux/Auth-reducer";
+import {Redirect} from "react-router-dom";
+
 
 
 const Login = (props) => {
-    return <div>
 
-        <h1> Login will be here </h1>
+    const onSubmit = (data) => {
+        props.loginThunkCreator(data.email, data.password,data.rememberMe)
+
+    };
+
+    if (props.isAuth) {
+        return <Redirect to={'/profile'}/>
+    }
+
+
+
+    return <div className={style.login} >
+
+        <h1> Login </h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+
+
+
     </div>
 };
 
 
-export default Login;
+
+const mapStateToProps = (state) => {{
+   return {
+       isAuth:state.auth.isAuth
+   }
+
+}};
+
+export default connect (mapStateToProps, {loginThunkCreator}) (Login);
